82cf67c304af2caae7fea80e58b5b91098560dbc
AbhinavGunti <78372006+AbhinavGunti@users.noreply.github.com>
2023-02-14 13:47:56 +0530

    Second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout <<"Name : Abhinav Gunti\n";
+  cout <<"SRN : PES1UG20CS008\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
