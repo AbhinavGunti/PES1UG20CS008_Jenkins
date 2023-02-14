13995f49760c0886bc92bb7490816e1bb6fcb180
AbhinavGunti <78372006+AbhinavGunti@users.noreply.github.com>
2023-02-14 14:06:24 +0530

    Second Commit

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+  cout <<"Name : Abhinav Gunti\n";
+  cout <<"SRN : PES1UG20CS008\n";
+  cout <<"Assignment 2 – Jenkins\n";
+  return 0;
+}
